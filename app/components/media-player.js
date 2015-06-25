2bf9fb3397e7b35974ab6a3d973b1abb73bc9fc9
Matt Chastain <matthewdchastain@gmail.com>
2015-06-24 21:35:06 -0400

    This commit is brought to you, in part, by Kyle Richardson - were it not for his aid in locating the methods for setting volumes, etc...

--- app/components/media-player.js
+++ app/components/media-player.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
 
 	init: function() {
 		this._super();
-		SC.get('/tracks/fancy', {limit: 1}, function(song) {
+		SC.get('/tracks/word', {limit: 1}, function(song) {
 			this.set('song', song);
 			this.set('id', song.id);
 			console.log(song);
@@ -19,14 +19,20 @@ export default Ember.Component.extend({
 		playSong: function() {
 			if (!this.haveSong) {
 				SC.stream(("/tracks/" + this.get('id')), function(sound){
-					this.sound = sound;
+					this.set('sound', sound);
 				  	this.sound.play();
-				  	this.set('current_time', this.sound._player._currentPosition);
-				  	this.set('time_left', this.sound._player._duration - this.sound._player._currentPosition);
+				  	var ms = this.sound._player._currentPosition,
+					   min = Math.floor(ms/1000/60),
+					   sec = ('0' +(ms/1000) % 60).slice(-2);
+				  	this.set('current_time', ''+min + ':' + sec);
+				  	var ms = this.sound._player._duration - this.sound._player._currentPosition,
+					   min = Math.floor(ms/1000/60),
+					   sec = Math.floor(('0' +(ms/1000) % 60).slice(-2));
+				  	this.set('time_left', ''+min + ':' + sec);
 				}.bind(this));
 				$('.play-logo > .fa-play-circle-o').hide();
 				$('.play-logo > .fa-pause').show();
-				this.haveSong = true;
+				this.set('haveSong', true);
 			} else {
 				this.get('sound').play();
 				$('.play-logo > .fa-play-circle-o').hide();
@@ -37,6 +43,43 @@ export default Ember.Component.extend({
 			this.get('sound').pause();
 			$('.play-logo > .fa-pause').hide();	
 			$('.play-logo > .fa-play-circle-o').show();
+		},
+		volumeUp: function() {
+			if (volume === 1) {
+				return false;
+			} else {
+				var volume = this.sound.getVolume();
+				this.sound.setVolume(volume + .1);
+				console.log(this.sound.getVolume());
+			}
+		},
+		volumeDown: function() {
+			if (volume < 0.1) {
+				return false;
+			} else {
+				var volume = this.sound.getVolume();
+				this.sound.setVolume(volume-.1);
+				console.log(this.sound.getVolume());
+			}
+		},
+		replay: function() {
+			this.get('sound').stop();
+			SC.stream(("/tracks/" + this.get('id')), function(sound){
+				this.set('sound', sound);
+			  	this.sound.play();
+			  	var ms = this.sound._player._currentPosition,
+				   min = Math.floor(ms/1000/60),
+				   sec = ('0' +(ms/1000) % 60).slice(-2);
+			  	this.set('current_time', ''+min + ':' + sec);
+			  	var ms = this.sound._player._duration - this.sound._player._currentPosition,
+				   min = Math.floor(ms/1000/60),
+				   sec = Math.floor(('0' +(ms/1000) % 60).slice(-2));
+			  	this.set('time_left', ''+min + ':' + sec);
+			}.bind(this));
+			$('.play-logo > .fa-play-circle-o').hide();
+			$('.play-logo > .fa-pause').show();
+			this.set('haveSong', true);
+			
 		}
 	}
 
